ebd430bfed52378b89b7884d8778fe71a70c2b52
jose fusil <josealejandrofusil2007@gmail.com>
2023-10-21 12:40:53 -0500

    clase_1 listo

---
+++ index.js
@@ -0,0 +1,7 @@
+
+alert("Esto es una advertencia")
+let confirmacin = confirm("¿Estas seguro de que confias en el proceso?")
+console.log(confirmacin)
+
+let marca = prompt("que marca de zapatos te gustan")
+console.log(marca)
