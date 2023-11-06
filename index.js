24b7c905ebc9dd331b510a4309d06e91de8f7be6
jose fusil <josealejandrofusil2007@gmail.com>
2023-11-06 10:31:30 -0500

    juego terminado

--- index.js
+++ index.js
@@ -67,37 +67,85 @@ const juego = {
     2: `piedra`,
     3: `papel`,
 }
- let userChoose = parseInt(prompt (`Elige una opcion: 
+let user
+let pc
+let puntosUser=0
+let puntosPc=0
+
+function pedirNUm () {
+let userChoose = parseInt(prompt (`Elige una opcion: 
                             \n1. Tijera
                             \n2. Piedra
                             \n3. Papel`))
 console.log (userChoose)
-let user = juego[userChoose]
+user = juego[userChoose]
 console.log(user)
 alert (`El usuario escogio ${user}`)
+}
+
+function numPC () {
 let pcChoose = Math.round(Math.random()*2) + 1
-let pc = juego[pcChoose]
+pc = juego[pcChoose]
 console.log(pc)
 alert (`la pc escogio ${pc}`)
+}
+
 let win= "¡¡¡Ganaste!!!"
 let losed="Perdiste... Intenta otra vez"
+
+function ganador () {
 switch (user) {
+    case pc:
+        alert("Empate")
+        break
     case 'tijera':
-        if (pc === juego[2]){alert(losed)}
-        else if(pc ===juego[3]) {alert(win)}
-        else{alert(`empate`)}
+        if (pc === juego[2]){
+            alert(losed)
+            puntosPc++
+        }
+        else if(pc ===juego[3]) {
+            puntosUser++
+            alert(win)}
         break;
     case 'piedra':
-        if (pc === juego[3]){alert(losed)}
-        else if(pc ===juego[1]) {alert(win)}
-        else{alert(`empate`)}
+        if (pc === juego[3]){
+            alert(losed)
+            puntosPc++
+        }
+        else if(pc ===juego[1])  {
+            puntosUser++
+            alert(win)}
         break;
     case 'papel' :
-        if (pc === juego[1]){alert(losed)}
-        else if(pc ===juego[2]) {alert(win)}
-        else{alert(`empate`)}
+        if (pc === juego[1]){
+            alert(losed)
+            puntosPc++
+        }
+        else if(pc ===juego[2])  {
+            puntosUser++
+            alert(win)}
         break;
     default:
         alert('escriba un codigo correcto')
         break;
+}
+}
+let ganadorpc  = 'El ganador fue la pc.'
+let ganadoruser = 'Felecidades ganaste el juego'
+for (let i = 0; i < 3; i++) {
+    console.log(`vuelta numero ${i}`)
+    pedirNUm()
+    numPC()
+    ganador()
+    let calcularGanador = function() {
+        if (puntosPc === 2) {
+            alert (ganadorpc)
+            return 'gano la pc'
+        } else if (puntosUser === 2) {
+            alert(ganadoruser)
+            return 'gano el usuario'
+        }
+    }
+    calcularGanador()
+    
 }
